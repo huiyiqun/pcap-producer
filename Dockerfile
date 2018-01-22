59387bbe6e6781ed14a37763547953a5293adf55
Hui Yiqun <huiyiqun@gmail.com>
2018-01-22 17:26:15 +0800

    WIP

--- Dockerfile
+++ Dockerfile
@@ -7,10 +7,15 @@ RUN wget http://apt-stable.ntop.org/16.04/all/apt-ntop-stable.deb && dpkg -i apt
 # Install latest go
 RUN wget https://dl.google.com/go/go1.9.2.linux-amd64.tar.gz -O- | tar xvz -C /opt
 
-# Install gopacket
+# Install dependencies
 RUN apt-get update && apt-get install -y pfring git
 ENV GOPATH=/go
 RUN /opt/go/bin/go get github.com/google/gopacket/examples/pfdump
 RUN /opt/go/bin/go get github.com/segmentio/kafka-go
 
-ENTRYPOINT ['/go/bin/pfdump']
+RUN mkdir /opt/pcap-producer
+WORKDIR /opt/pcap-producer
+ADD main.go .
+RUN /opt/go/bin/go build main.go
+
+ENTRYPOINT ["./main"]
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"flag"
+	"github.com/google/gopacket/pfring"
+	"log"
+)
+
+func main() {
+	var err error
+
+	iface := flag.String("i", "bond0", "Interface to read packets from")
+	snaplen := flag.Int("s", 65536, "Sanp length (number of bytes max to read per packet")
+	flag.Parse()
+
+	ring, err := pfring.NewRing(*iface, uint32(*snaplen), pfring.FlagPromisc)
+	if err != nil {
+		log.Fatalln("pfring ring creation error:", err)
+	}
+
+	err = ring.SetSocketMode(pfring.ReadOnly)
+	if err != nil {
+		log.Fatalln("pfring SetSocketMode error:", err)
+	}
+
+	err = ring.Enable()
+	if err != nil {
+		log.Fatalln("pfring Enable error:", err)
+	}
+}
