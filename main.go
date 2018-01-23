48bd5984c702281d00c7b7d51b2831cb6a6d9b6d
Hui Yiqun <huiyiqun@gmail.com>
2018-01-23 13:15:14 +0800

    print_stats

--- main.go
+++ main.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"github.com/google/gopacket"
 	"github.com/google/gopacket/layers"
 	"github.com/google/gopacket/pfring"
+	kafka "github.com/segmentio/kafka-go"
 	"log"
+	"time"
 )
+
+func print_stats(r *pfring.Ring) {
+	for range time.Tick(1 * time.Second) {
+		stats, err := r.Stats()
+		if err != nil {
+			log.Fatalln("pfring ring stats error:", err)
+		}
+		log.Printf("recv/drop: %d/%d | drop%%: %f%%", stats.Received, stats.Dropped, float64(stats.Dropped)/float64(stats.Received))
+	}
+}
 
 func main() {
 	var err error
@@ -30,6 +43,7 @@ func main() {
 		log.Fatalln("pfring Enable error:", err)
 	}
 
+	go print_stats(ring)
 	source := gopacket.NewPacketSource(ring, layers.LayerTypeEthernet)
 	for packet := range source.Packets() {
 		log.Printf("Packet %d: %v", count, packet.Data())
